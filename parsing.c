674169278083270a1623137adb687fcdee58f69a
wangwangwar <wangwangwar@wangwangwardeMacBook-Pro.local>
2015-01-25 21:39:01 +0800

    Chapter 7 Evaluation

--- parsing.c
+++ parsing.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <editline/readline.h>
 #include "mpc/mpc.h"
 
+long eval(mpc_ast_t* t);
+long eval_op(long x, char *op, long y);
+
+long eval(mpc_ast_t* t) {
+    /* If tagged as number return it directly. */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child. */
+    char *op = t->children[1]->contents;
+
+    /* We store the third child in `x` */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char *op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
 
 int main(int argc, char **argv) {
     /* Create Some Parsers */
@@ -37,6 +71,10 @@ int main(int argc, char **argv) {
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             /* On Success Print the AST */
             mpc_ast_print(r.output);
+
+            long result = eval(r.output);
+            printf("%li\n", result);
+
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise Print the Error */
