4d2136cbe8d23e2a18bf542690ae99a1e64c9985
wangwangwar <wangwangwar@wangwangwardeMacBook-Pro.local>
2015-03-07 16:41:24 +0800

    Chapter 9 - Evaluating Expressions

--- parsing.c
+++ parsing.c
@@ -21,11 +21,7 @@ enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 /* Create Enumeration of Possible Error Types */
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-
-lval* eval(mpc_ast_t* t);
-
-/* Use operator string to see which operation to perform */
-lval* eval_op(lval x, char *op, lval y);
+/****** Constructor ******/
 
 /* Create a new number type lval */
 lval* lval_num(long x);
@@ -38,6 +34,8 @@ lval* lval_sym(char *s);
 
 /* A pointer to a new empty Sexpr lval */
 lval* lval_sexpr(void);
+
+/******* Operation ******/
 
 lval* lval_read_num(mpc_ast_t* t);
 
@@ -53,6 +51,16 @@ void lval_println(lval* v);
 
 /* Delete lval */
 void lval_del(lval* v);
+
+lval *lval_eval_sexpr(lval* v);
+
+lval* lval_eval(lval* v);
+
+lval* lval_pop(lval* v, int i);
+
+lval* lval_take(lval* v, int i);
+
+lval* builtin_op(lval* a, char* op);
 
 
 int main(int argc, char **argv) {
@@ -91,7 +99,7 @@ int main(int argc, char **argv) {
             /* On Success Print the AST */
             mpc_ast_print(r.output);
 
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
 
@@ -225,4 +233,104 @@ void lval_del(lval* v) {
     
     /* Fee the memory allocated for the "lval" struct itself */
     free(v);
+}
+
+lval *lval_eval_sexpr(lval* v) {
+    /* Evaluate Children */
+    for (int i = 0; i < v->count; i++) {
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
+
+    /* Error Checking */
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+    }
+
+    /* Empty Expression */
+    if (v->count == 0) { return v; }
+
+    /* Single Expression */
+    if (v->count == 1) { return lval_take(v, 0); }
+
+    /* Ensure First Element is Symbol */
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f); lval_del(v);
+        return lval_err("S-expression Does not start with symbol!");
+    }
+
+    /* Call builtin with operator */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    /* Evaluate Sexpressions */
+    if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+    /* All other lval types remain the same */
+    return v;
+}
+
+lval* lval_pop(lval* v, int i) {
+    /* Find the item at "i" */
+    lval* x = v->cell[i];
+
+    /* Shift memory after the item at "i" over the top */
+    memmove(&v->cell[i], &v->cell[i+1],
+            sizeof(lval*) * (v->count-i-1));
+
+    /* Decrease the count of items in the list */
+    v->count--;
+
+    /* Reallocate the memory used */
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+
+    return x;
+}
+
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
+lval* builtin_op(lval* a, char* op) {
+    /* Ensure all arguments are numbers */
+    for (int i = 0; i < a->count; i++) {
+        if (a->cell[i]->type != LVAL_NUM) {
+            lval_del(a);
+            return lval_err("Cannot operate on non-number!");
+        }
+    }
+
+    /* Pop the first element */
+    lval* x = lval_pop(a, 0);
+
+    /* If no arguments and sub then perform unary negation */
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = -x->num;
+    }
+
+    /* While there are still elements remaining */
+    while (a->count > 0) {
+        /* Pop the next element */
+        lval* y = lval_pop(a, 0);
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+            if (y->num == 0) {
+                lval_del(x); lval_del(y);
+                x = lval_err("Division By Zero!"); break;
+            }
+            x->num /= y->num;
+        }
+
+        lval_del(y);
+    }
+
+    lval_del(a); return x;
 }
